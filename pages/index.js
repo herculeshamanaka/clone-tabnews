fc71dbece044364e99c6ab2da63791cbafaaad14
Herculìs Ḧ <hercules.hamanaka@gmail.com>
2024-11-11 22:23:23 +0000

    fix: removing Head tag

--- pages/index.js
+++ pages/index.js
@@ -70,11 +70,6 @@ function Home() {
 
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Under Construction</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>Under Construction</h1>
         <p className={styles.description}>
