eb9dc3e317f84cea6d08a0402530a614cf12f3d5
Herculìs Ḧ <hercules.hamanaka@gmail.com>
2024-11-11 22:20:23 +0000

    fix: change styles

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,75 @@
 import styles from "../styles/Home.module.css";
 
 function Home() {
+  const styles = {
+    container: {
+      display: "flex",
+      justifyCcontent: "center",
+      alignItems: "center",
+      height: "100vh",
+      margin: "0",
+      fontFamily: "Arial, sans-serif",
+      backgroundColor: "#f3f4f6",
+      textAlign: "center",
+    },
+    main: {
+      textAlign: "center",
+    },
+    title: {
+      fontSize: "3em",
+      color: "#333",
+    },
+    description: {
+      fontSize: "1.2em",
+      color: "#666",
+    },
+    devNews: {
+      marginTop: "20px",
+    },
+    gorilla: {
+      marginTop: "20px",
+      position: "relative",
+      width: "200px",
+      height: "200px",
+      backgroundColor: "#333",
+      borderRadius: "50%",
+    },
+    eye: {
+      position: "absolute",
+      width: "20px",
+      height: "20px",
+      backgroundColor: "#fff",
+      borderRadius: "50%",
+      top: "50px",
+    },
+    left: {
+      left: "50px",
+    },
+    right: {
+      right: "50px",
+    },
+    nose: {
+      position: "absolute",
+      width: "40px",
+      height: "40px",
+      backgroundColor: "#000;",
+      borderRadius: "50%",
+      top: "100px",
+      left: "50%",
+      transform: "translateX(-50%)",
+    },
+    mouth: {
+      position: "absolute",
+      width: "100px",
+      height: "50px",
+      backgroundColor: "#000",
+      borderRadius: "0 0 50px 50px",
+      top: "140px",
+      left: "50%",
+      transform: "translateX(-50%)",
+    },
+  };
+
   return (
     <div className={styles.container}>
       <Head>
